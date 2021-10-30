dced7a2b8e9e8514824d4b3e338cab49b85d4ca8
Mateus Andriola <mateusandriola18@gmail.com>
2021-10-30 16:31:41 -0300

    feat(shippingbag): component to shows productList selected and redirect user to cart page

---
+++ src/components/Atoms/ShippingBag/index.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import shoppinBagIcon from '../../../assets/icons/shopping-bag.svg';
+import { Container } from './styles';
+
+export const ShippingBag = () => (
+  <Link href="/cart" passHref>
+    <Container>
+      <a>
+        <Image src={shoppinBagIcon} width={24} height={24} />
+      </a>
+      <span>2</span>
+    </Container>
+  </Link>
+);
---
+++ src/components/Atoms/ShippingBag/styles.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin-left: 2.7rem;
+  position: relative;
+
+  > * {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  > span {
+    width: 1.7rem;
+    height: 1.7rem;
+    color: #FFFFFF;
+
+    font-size: 1rem;
+    font-weight: 500;
+    border-radius: 50%;
+    background-color: ${(props) => props.theme.colors.others.delete};
+
+    right: 0;
+    bottom: 0;
+    position: absolute;
+    transform: translate(50%, 50%);
+  }
+`;
