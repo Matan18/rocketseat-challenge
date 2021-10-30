85832b666b916630af7601f1a5bde205d51466e6
Mateus Andriola <mateusandriola18@gmail.com>
2021-10-30 14:54:21 -0300

    fix(logo): link & styles error
    accourding https://nextjs.org/docs/messages/link-passhref there is an error when using Link wrapping
    custom component, followed docs using styled-components too

--- src/components/Atoms/Logo/index.tsx
+++ src/components/Atoms/Logo/index.tsx
@@ -1,3 +1,8 @@
+import Link from 'next/link';
 import { LogoText } from './styles';
 
-export const Logo = () => <LogoText href="/"><a>capputeeno</a></LogoText>;
+export const Logo = () => (
+  <Link href="/" passHref>
+    <LogoText><h1>capputeeno</h1></LogoText>
+  </Link>
+);
--- src/components/Atoms/Logo/styles.ts
+++ src/components/Atoms/Logo/styles.ts
@@ -1,9 +1,10 @@
 import styled from 'styled-components';
-import Link from 'next/link';
 
-export const LogoText = styled(Link)`
-  font-size: 40px;
-  line-height: 1.5em;
-  color: ${(props) => props.theme.colors.text.logo};
-  font-family: ${(props) => props.theme.fonts.sairaStencileOne};
+export const LogoText = styled.a`
+  h1 {
+    font-size: 40px;
+    line-height: 150%;
+    color: ${(props) => props.theme.colors.text.logo};
+    font-family: ${(props) => props.theme.fonts.sairaStencileOne};
+  }
 `;
