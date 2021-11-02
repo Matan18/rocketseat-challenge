61d9da7b3cfd8def19fc9bfc3c521ee4d3d1534d
Mateus Andriola <mateusandriola18@gmail.com>
2021-11-02 17:54:19 -0300

    feat(pagenavigators): using most hook functions

--- src/components/Molecules/PageNavigators/index.tsx
+++ src/components/Molecules/PageNavigators/index.tsx
@@ -1,24 +1,45 @@
 import { PageButton } from '@components/Atoms/PageButton';
+import { useProducts } from '@hooks/useProducts';
 import Image from 'next/image';
 import ChevronDown from '../../../assets/icons/chevron-down.svg';
 import { Container } from './styles';
 
-type Props = {
-  pages: number;
-}
+export const PageNavigators = () => {
+  const {
+    pages,
+    nextPage,
+    setParams,
+    previousPage,
+    params: { page },
+  } = useProducts();
 
-export const PageNavigators = ({ pages }: Props) => (
-  <Container>
-    {Array
-      .from({ length: pages }, (_, i) => i)
-      .map((item) => (
-        <PageButton key={item} isSelect={item + 1 === 1}>{item + 1}</PageButton>
-      ))}
-    <PageButton>
-      <Image className="left" src={ChevronDown} width={20} height={20} />
-    </PageButton>
-    <PageButton>
-      <Image className="right" src={ChevronDown} width={20} height={20} />
-    </PageButton>
-  </Container>
-);
+  const exactPage = (pageIndex: number) => {
+    setParams((prev) => ({
+      ...prev,
+      page: pageIndex + 1,
+    }));
+  };
+
+  return (
+    <Container>
+      {Array
+        .from({ length: pages }, (_, i) => i)
+        .map((item) => (
+          <PageButton
+            key={item}
+            onClick={() => exactPage(item)}
+            isSelect={item + 1 === page}
+          >
+            {item + 1}
+
+          </PageButton>
+        ))}
+      <PageButton onClick={previousPage}>
+        <Image className="left" src={ChevronDown} width={20} height={20} />
+      </PageButton>
+      <PageButton onClick={nextPage}>
+        <Image className="right" src={ChevronDown} width={20} height={20} />
+      </PageButton>
+    </Container>
+  );
+};
--- src/components/Organisms/ProductListContainer/index.tsx
+++ src/components/Organisms/ProductListContainer/index.tsx
@@ -5,19 +5,17 @@ import { useProducts } from '@hooks/useProducts';
 import { Container } from './styles';
 
 export const ProductListContainer = () => {
-  const { products, meta: { count } } = useProducts();
-
-  const pages = count / 12;
+  const { products } = useProducts();
 
   return (
     <Container>
-      <PageNavigators pages={pages} />
+      <PageNavigators />
       <ul>
         {products.map((product) => (
           <ProductItem key={product.id} product={product} />
         ))}
       </ul>
-      <PageNavigators pages={pages} />
+      <PageNavigators />
     </Container>
   );
 };
